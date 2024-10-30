e414fd6a7aa2c01be4b4a22d902feef566091240
josef butts <buttsjosef@gmail.com>
2024-10-29 22:44:57 -0400

    Revert "fix formatting"
    This reverts commit b97593342ccee8c386f3819fad03dea0f6571d63.

--- src/embeddings.py
+++ src/embeddings.py
@@ -3,12 +3,14 @@
 import streamlit as st
 from langchain_community.embeddings import OpenAIEmbeddings
 
-
 def get_embeddings(query: str):
     """Get embeddings based on selected provider."""
-    if st.session_state.embedding_type == "openai":
+    if st.session_state.embedding_type == 'openai':
         embedding = OpenAIEmbeddings(api_key=st.session_state.openai_api_key)
         return embedding.embed_query(query)
     else:
         # Use Qdrant's built-in FastEmbed
-        return {"name": "fastembed", "query": query}
+        return {
+            "name": "fastembed",
+            "query": query
+        }
